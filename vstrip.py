c9f74707ae94f9c5e369fc2268dd978da1a06566
Chinmayi-Prasad <chinmayi2398@gmail.com>
2019-03-18 22:37:32 +0530

    Saving all lines without error

--- vstrip.py
+++ vstrip.py
@@ -86,6 +86,7 @@ def segmentStrips(strips):
 
     final_px = []
     # j is indiv strip no
+    #After finding the mid point of each line we eliminate the unnecesary ones.
     for j in range(len(mid_arr)):
         z = []
         for i in range(len(mid_arr[j])):
@@ -111,11 +112,14 @@ def GetLinesPxls(final_px): #Cleaning the final_pxls array
         medians.append(med)
         print(med , len(new_line))
         for nl in range(len(new_line)-1):
-            if(new_line[nl]>med+50 or new_line[nl]<med-50):
-                if(nl==0):
+            # if(new_line[nl]>med+50 or new_line[nl]<med-50):
+            #     new_line[nl] = 0
+            # el
+            if(new_line[nl]>med+30 or new_line[nl]<med-30):
+                if(nl==0): #First Strip 
                     new_line[nl] = new_line[nl+1]
-                elif(nl==len(new_line)-1):
-                    new_line[nl] = new_line[nl-1]
+                elif(nl==len(new_line)-1): #Last Strip
+                    new_line[nl] = med
                 else:
                     new_line[nl] = (new_line[nl+1]+new_line[nl-1])/2
         line_px.append(new_line)
@@ -131,6 +135,7 @@ def combineStrips(line_px , medians , image):
     print(medians)
     for l_no in range(len(line_px)):
         images_in_line = []
+        prev_start_px = 0 if l_no==0 else int(line_px[l_no-1][j])
         for j in range(len(line_px[l_no])):
             s = j*s_width
             if(s+s_width<cols):
@@ -142,46 +147,62 @@ def combineStrips(line_px , medians , image):
         images_opened = []
         for a in range(len(images_in_line)):
             im_a = np.array(images_in_line[a])
-            cv2.imwrite('op/line'+ str(l_no) + "_" +str(a)+'.jpg', im_a)
-            images_opened.append(Image.open('op/line'+ str(l_no) + "_" + str(a)+'.jpg'))
+            # print("Image is " , im_a , images_in_line[a])
+            # if(len(im_a)==0):
+            #     break
+            if(len(im_a)!=0):
+                cv2.imwrite('op/line'+ str(l_no) + "_" +str(a)+'.jpg', im_a)
+                images_opened.append(Image.open('op/line'+ str(l_no) + "_" + str(a)+'.jpg'))
+            else:
+                temp_im = Image.new('RGB', (100,2))
+                images_opened.append(temp_im)
 
-        widths, heights = zip(*(i.shape for i in images_in_line))
+        # print(images_opened)
+        widths, heights = zip(*(i.size for i in images_opened))
         total_width = sum(widths) 
         max_height = max(heights)
 
         new_im = Image.new('RGB', (total_width, max_height))
         x_offset = 0
-
-        print("Image Line 1" , images_opened[1])
         strip_cnt = 0
         for im in images_opened:
-            np_im = np.array(im)
-            nxt_x_offset = x_offset + np_im.shape[1]
-            box = (x_offset, 0 , nxt_x_offset ,np_im.shape[0])
-            #box = (x_offset, 0,100 ,90)
-            print(np_im.shape , box)
+            # np_im = np.array(im)
+            nxt_x_offset = x_offset + im.size[0]
+            box = (x_offset, 0 , nxt_x_offset ,im.size[1])
+            # print(np_im.shape , box)
             new_im.paste(im, box)
             x_offset = nxt_x_offset
             strip_cnt  = strip_cnt+1
-        
-        new_im.save('op/test/'+img_str+'_l'+str(l_no)+'.jpg')
+
+        # print("Image is:" , new_im)
+        ni = np.array(new_im)
+        print(ni.shape , l_no , total_width , heights)
+        if(len(ni.shape)== 3):
+            new_im.save('op/test/'+img_str+'_l'+str(l_no)+'.jpg')
 
 if __name__ == "__main__":
-    img_str = "41D3"
+    img_str = "74D2"
     image = cv2.imread('images/'+img_str+'.jpg')
     im = cv2.imread('images/'+img_str+'.jpg')
     image = preprocessImage(image)
-
-    strips = obtainStrips(image)
-    final_px , mid_arr = segmentStrips(strips)
+    im = preprocessImage(im)
+    # strips = obtainStrips(image)
+    # final_px , mid_arr = segmentStrips(strips)
 
     # pickle_out = open("One.pickle","wb")
     # pickle.dump(final_px, pickle_out)
     # pickle_out.close()
     
-    # pickle_in = open("One.pickle","rb")
-    # final_px = pickle.load(pickle_in)
+    pickle_in = open("One.pickle","rb")
+    final_px = pickle.load(pickle_in)
 
     line_px , medians = GetLinesPxls(final_px)
-    combineStrips(line_px , medians , image)
-    cv2.imwrite('op/'+img_str+'.jpg',image)
+
+    for j in range(len(line_px)):
+            for i in range(len(line_px[j])):
+                l_px = int(line_px[j][i])
+                cv2.line(im,(s_width*i,l_px),(s_width*(i+1),l_px),(255,0,0),thickness=1)
+
+
+    combineStrips(line_px , medians , im)
+    cv2.imwrite('op/'+img_str+'.jpg',im)
